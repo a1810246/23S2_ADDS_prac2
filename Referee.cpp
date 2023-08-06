d61dcdc4afc3dc70e8dea99cb8a9e9aad67a6cc7
a1810246 <a1810246@student.adelaide.edu.au>
2023-08-06 12:36:23 +0000

    test

---
+++ Referee.cpp
@@ -0,0 +1,29 @@
+#include "Referee.h"
+#include <iostream>
+
+Referee::Referee()
+{
+
+}
+
+Player* Referee::refGame(Player* player1, Player* player2)
+{
+    Move* p1 = player1->makeMove();
+    Move* p2 = player2->makeMove();
+
+    p1->compare(p2);
+    if(p1->getFlag()==1)
+    {
+        return player1;
+    }
+
+    else if(p1->getFlag()==2)
+    {
+        return nullptr;
+    }
+
+    else
+    {
+        return player2;
+    }
+}
