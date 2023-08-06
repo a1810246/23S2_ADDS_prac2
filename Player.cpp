8407dcf1cbd218c0a00e2eb3309c89fd4f51f467
a1810246 <a1810246@student.adelaide.edu.au>
2023-08-06 12:15:43 +0000

    tes

---
+++ Player.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+
+#include "Rock.h"
+#include "Paper.h"
+#include "Scissors.h"
+#include "Monkey.h"
+#include "Pirate.h"
+#include "Ninja.h"
+#include "Robot.h"
+#include "Zombie.h"
+
+using namespace std;
+
+Player::Player()
+{
+    name = "";
+}
+
+Player::Player(string name):name(name){
+
+}
+
+string Player::getName()
+{
+    return name;
+}
+
+void Player::SetName(string name)
+{
+    this->name = name;
+}
+
+Move* Player::returnMove(string moveName)
+{
+    if(moveName == "Rock")
+    {
+        return new Rock();
+    }
+    else if(moveName == "Scissors")
+    {
+        return new Scissors();
+    }
+    else if(moveName == "Paper")
+    {
+        return new Paper();
+    }
+    else if(moveName == "Monkey")
+    {
+        return new Monkey();
+    }
+    else if(moveName == "Pirate")
+    {
+        return new Pirate();
+    }
+    else if(moveName == "Ninja")
+    {
+        return new Ninja();
+    }
+    else if(moveName == "Robot")
+    {
+        return new Robot();
+    }
+
+    return nullptr;
+}
+
