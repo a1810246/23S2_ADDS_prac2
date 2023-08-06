8fbf079e3396bb1d89604ec5e5a8aa579d9911be
a1810246 <a1810246@student.adelaide.edu.au>
2023-08-06 14:08:04 +0000

    test

---
+++ Pirate.cpp
@@ -0,0 +1,14 @@
+#include"Pirate.h"
+
+Pirate::Pirate() : Move("Pirate")
+{
+
+}
+
+void Pirate::compare(Move* move)
+{
+    if(move->getName() == "Robot" || move->getName() == "Monkey")
+    {
+        flag = 1;
+    }
+}
